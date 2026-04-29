d7031036963ce5fdaaec0f61ed1574aa590dc81f
Dag-Erling Smørgrav <des@des.dev>
2026-04-29 19:53:26 +0200

    db: Fix results view
    The drop command had the wrong name, and the view would include
    candidates who had received no votes if fewer than nine candidates
    had received any votes at all.

--- db/fbce.sql
+++ db/fbce.sql
@@ -44,10 +44,11 @@ create table core_votes (
         foreign key(candidate) references persons(id) on delete cascade on update cascade
 );
 
-drop view if exists core_results;
+drop view if exists results;
 create view results as
     select persons.id, persons.login as login, persons.realname as realname, persons.incumbent, count(core_votes.*) as votes
     from persons join core_votes on persons.id = core_votes.candidate
+    where votes > 0
     group by persons.id, persons.login, persons.realname, persons.incumbent;
 
 --
